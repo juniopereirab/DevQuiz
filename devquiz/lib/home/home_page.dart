eac72a9e2dd1cfd8c52f968d0ca521708ddd0961
Dâmaso Júnio <junio.pereirab@gmail.com>
2021-05-02 01:57:02 -0300

    Fix layout errors and finish the home page

--- devquiz/lib/home/home_page.dart
+++ devquiz/lib/home/home_page.dart
@@ -16,9 +16,12 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBarWidget(),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
+            SizedBox(
+              height: 24,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -36,7 +39,21 @@ class _HomePageState extends State<HomePage> {
                 ),
               ],
             ),
-            QuizCardWidget()
+            SizedBox(
+              height: 24,
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
--- devquiz/lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ devquiz/lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -24,26 +24,26 @@ class QuizCardWidget extends StatelessWidget {
             child: Image.asset(AppImages.blocks),
           ),
           SizedBox(
-            height: 24,
+            height: 20,
           ),
           Text(
             "Gerenciamento de Estado",
             style: AppTextStyles.heading15,
           ),
           SizedBox(
-            height: 24,
+            height: 16,
           ),
           Row(
             children: [
               Expanded(
                 flex: 1,
                 child: Text(
-                  "3 de 10",
+                  "3/10",
                   style: AppTextStyles.body11,
                 ),
               ),
               Expanded(
-                flex: 4,
+                flex: 3,
                 child: LinearProgressIndicator(
                   value: 0.3,
                   backgroundColor: AppColors.chartSecondary,
